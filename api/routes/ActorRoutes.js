bfba302aa9a909e30287e66b37796a1924956309
solzycoded <solomonfidelis012@gmail.com>
2024-05-02 19:34:27 +0100

    included the routes for actor

---
+++ api/routes/ActorRoutes.js
@@ -0,0 +1,14 @@
+import ActorController from "../controllers/ActorController.js";
+import express from "express"
+
+const router = express.Router();
+
+// Define routes
+router.post('/', ActorController.createActor);
+
+// export routes
+let actorRoutes = {
+    router
+}
+
+export default actorRoutes;
