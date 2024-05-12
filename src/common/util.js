276a36a4236430313cc04994991ad7ca00ff7d4a
solzycoded <solomonfidelis012@gmail.com>
2024-05-12 22:49:08 +0100

    finished display the correct data for a selected movie

--- src/common/util.js
+++ src/common/util.js
@@ -1,3 +1,60 @@
+
+// convert minutes to hours and minutes format
+const convertMinsToHours = mins => {
+    let h = Math.floor(mins / 60);
+    let m = Math.round(mins % 60);
+
+    return `${h} hr(s), ${m} min(s)`;
+};
+
+// created a new date in the format (dayofweek, day month year)
+// const formatDate = oldDate => {
+//     const date = new Date(oldDate);
+
+//     let dayOfWeek = getDayOfWeek(date.getDay()).slice(0, 3);
+//     let monthOfYear = getMonthOfYear(date.getMonth()).slice(0, 3);
+
+//     let newDate = `${dayOfWeek}, ${date.getDate()} ${monthOfYear} ${date.getFullYear()}`;
+
+//     return newDate;
+// };
+
+// const getDayOfWeek = day => {
+//     let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+//     return daysOfWeek[day];
+// };
+
+// const getMonthOfYear = month => {
+//     let monthsOfYear = [
+//         'January',
+//         'February',
+//         'March',
+//         'April',
+//         'May',
+//         'June',
+//         'July',
+//         'August',
+//         'September',
+//         'October',
+//         'November',
+//         'December'
+//     ];
+
+//     return monthsOfYear[month];
+// };
+
+const displayList = (list, separator) => {
+    return list.map((item, i) => {
+        return item.actor.name + ((i < list.length - 1) ? separator : "");
+    });
+};
+
+// Format a date string (Tue, 19 Mar 2024 16:27:17 GMT) to a human-readable format (12:32 PM, 19th March 2024)
+// const formatDateTime = date => {
+//     return format(new Date(date), 'p, do MMMM yyyy');
+// };
+
 const data = {
     movies: [
         {
@@ -67,6 +124,10 @@ const data = {
     ]
 }
 
-export default {
-    data
+const App = {
+    data,
+    convertMinsToHours,
+    displayList
 }
+
+export default App;
--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -5,7 +5,7 @@ import FetchRequest from "../assets/js/request/fetch"
 
 import MovieRating from '../components/MovieRating';
 import MovieGenreItem from '../components/MovieGenreItem';
-// import { formatDate, convertMinsToHours } from '../common/util';
+import App from "../common/util";
 
 import "../assets/css/MovieInfo.css"
 
@@ -18,76 +18,95 @@ function MovieInfo() {
 
     const [movie, setMovie] = useState({});
 
-    // useEffect(() => {
-    //     // Function to run when the component loads
-    //     getMovieDetails();
-    // }, []); // Empty dependency array ensures it runs only once on mount
+    useEffect(() => {
+        // Function to run when the component loads
+        getMovieDetails();
+    }, []); // Empty dependency array ensures it runs only once on mount
 
-    // const getMovieDetails = () => {
-    //     const success = (data) => {
-    //         setMovie(data);
-    //     }
+    const getMovieDetails = () => {
+        const success = (data) => {
+            setMovie(data);
+        }
 
-    //     const failure = (data) => {
-    //         setMovie(data);
-    //     }
+        const failure = (data) => {
+            setMovie(data);
+        }
 
-    //     (new FetchRequest("GET", `movies/${movie_name}`)).send(success, failure);
-    // }
+        (new FetchRequest("GET", `movies/${movie_name}`)).send(success, failure);
+    }
 
-    return (
+    return movie ? (
         <div className="shadow-lg p-3 mb-5 bg-body rounded">
             <div className="card border-0">
                 <div className="row">
                     <div className="col-12 col-lg-5 col-md-4 col-sm-12">
-                        <img src="/imgs/pic.jpg" className="img-fluid rounded w-100 movie-info-poster" alt="movie poster" />
+                        <img src={movie.poster} className="img-fluid rounded w-100 movie-info-poster" alt="movie poster" />
                     </div>
                     <div className="col-12 col-lg-7 col-md-8 col-sm-12">
                         <div className="card-body ps-0">
-                            <h5 className="card-title text-capitalize">name of movie</h5>
+                            <h5 className="card-title text-capitalize">{movie.name}</h5>
+                            {/* movie genres */}
                             <div className="container-fluid p-0 mb-2">
                                 <div className="row">
-                                    <div className="card-text col-4">
-                                        <NavLink to={'/genres/'} className="btn btn-link p-0 text-decoration-none">
-                                            <MovieGenreItem genre="something" />
-                                        </NavLink>
-                                    </div>
-                                    {/* {
-                                        movie.genres.map((genre, i) => {
-                                            return (
-                                                <div key={i} className="card-text col-4">
-                                                    <NavLink to={'/genres/' + genre.name} className="btn btn-link p-0 text-decoration-none">
-                                                        <MovieGenreItem genre={genre.trim()} />
-                                                    </NavLink>
-                                                </div>
-                                            );
-                                        })
-                                    } */}
+                                    {
+                                        (
+                                            (movie.genres) &&
+                                            movie.genres.map((item, i) => {
+                                                return (
+                                                    <div key={i} className="card-text col-2 movie-info-genre-item pe-0">
+                                                        <NavLink to={'/genres/' + item.genre.name} className="btn btn-link p-0 text-decoration-none">
+                                                            <MovieGenreItem genre={item.genre.name.trim()} />
+                                                        </NavLink>
+                                                    </div>
+                                                );
+                                            })
+                                        )
+                                    }
                                 </div>
                             </div>
-                            <MovieRating rating={5} />
-                            <div className="mt-3">
-                                <h6 className="card-title">Release Year</h6>
-                                {/* {formatDate(movie.release_date)} */}
-                                <p className="card-text">2023</p>
-                            </div>
+                            {/* movie rating */}
+                            {
+                                movie.rating && 
+                                (
+                                    <MovieRating rating={movie.rating} />
+                                )
+                            }
+                            {/* movie release year */}
+                            {
+                                movie.release_year && (<div className="mt-3">
+                                    <h6 className="card-title">Release Year</h6>
+                                    {/* {formatDate(movie.release_date)} */}
+                                    <p className="card-text">{movie.release_year}</p>
+                                </div>
+                                )
+                            }
 
                             <div className="mt-3">
                                 <h6 className="card-title">Runtime</h6>
-                                {/* {convertMinsToHours(movie.duration_minutes)} */}
-                                <p className="card-text">120 minutes</p>
+                                <p className="card-text">{App.convertMinsToHours(movie.runtime)}</p>
                             </div>
-                            <div className="mt-3">
-                                <h6 className="card-title">Language</h6>
-                                <p className="card-text">English</p>
-                            </div>
-                            <div className="mt-3">
-                                <h6 className="card-title">Cast</h6>
-                                <p className="card-text">Jennifer Lopez, Arnold Swatch, Jean Claude Van-Damme, Emma Stone</p>
-                            </div>
+                            {/* movie language */}
+                            {
+                                movie.language && (
+                                <div className="mt-3">
+                                    <h6 className="card-title">Language</h6>
+                                    <p className="card-text text-capitalize">{movie.language.name}</p>
+                                </div>
+                                )
+                            }
+                            {/* movie actors */}
+                            {
+                                (movie.actors && movie.actors.length > 0) && 
+                                (
+                                    <div className="mt-3">
+                                        <h6 className="card-title">Cast</h6>
+                                        <p className="card-text text-capitalize">{App.displayList(movie.actors)}</p>
+                                    </div>
+                                )
+                            }
                             <div className="mt-3">
                                 <h6 className="card-title">Watch Trailer</h6>
-                                <p className="card-text"><a href="https://www.youtube.com/watch?v=GyAKLundvvc&t=878s" target="_blank" className="text-decoration-none app-color">Watch "name of movie" trailer</a></p>
+                                <p className="card-text"><a href="https://www.youtube.com/watch?v=GyAKLundvvc&t=878s" target="_blank" className="text-decoration-none app-color">watch <b>{movie.name}</b> trailer</a></p>
                             </div>
                             
                             {
@@ -104,11 +123,13 @@ function MovieInfo() {
                     </div>
                     <div className="col-12 mt-3 movie-details-synopsis lg-screen">
                         <h6 className="card-title fs-4 mb-2">About Movie</h6>
-                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dicta doloremque eaque debitis. Dolor, perferendis non voluptas provident iusto sunt pariatur doloribus, dolorem excepturi asperiores consequuntur natus totam quos iure.</p>
+                        <p className="card-text">{movie.about}</p>
                     </div>
                 </div>
             </div>
         </div>
+    ) : (
+        <div className="fs-3 text-center">Movie name provided does not exist</div>
     );
 }
 
