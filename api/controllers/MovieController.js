8beaf01824c1a44d0866d14177c3214aa03e1b08
solzycoded <solomonfidelis012@gmail.com>
2024-05-18 14:32:39 +0100

    created an api that returns the details of a movie, when given it's id

--- api/controllers/MovieController.js
+++ api/controllers/MovieController.js
@@ -146,11 +146,35 @@ const getMovie = async (req, res) => {
     }
 };
 
+const findMovieById = async (req, res) => {
+    const { id } = req.params;
+
+    if(!id){
+        return res.status(500).json({ success: false, message: "Movie Id is missing!" });
+    }
+
+    try {
+        const movie = await Movie.findById(id)
+            .populate(App.createPopulateVal("genres", 'genre -_id', "genre", "name")) // genres
+            .populate(App.createPopulateVal("actors", 'actor -_id', "actor", "name")) // actors
+            .populate(App.createPopulateVal("categories", 'category -_id', "category", "name")) // categories
+            .populate("links", "link") // links
+            .populate('language', 'name -_id') // Populate the 'language' field in the Movie document
+            .exec();
+
+        const success = movie!=null;
+        res.status(201).json({ success: success, data: movie });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
 // export CONTROLLER FUNCTIONS
 export default {
     createMovie,
     listMovie,
     listMovieByCategory,
     listMovieByGenre,
-    getMovie
+    getMovie,
+    findMovieById
 }
--- api/routes/MovieRoutes.js
+++ api/routes/MovieRoutes.js
@@ -9,7 +9,9 @@ router.get('/', MovieController.listMovie);
 router.get('/by-category/:name', MovieController.listMovieByCategory);
 router.get('/by-genre/:name', MovieController.listMovieByGenre);
 router.get('/:name', MovieController.getMovie);
+router.get('/by-id/:id', MovieController.findMovieById);
 
+// define routes to create a new movie with an image
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 const myUploadMiddleware = upload.single("poster");
