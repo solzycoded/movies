7e5e6eb153f81eb1cb0d08d764382159c5edbc5f
solzycoded <solomonfidelis012@gmail.com>
2024-05-25 10:42:31 +0100

    fixed the nav bar issue on the admin dashboard

--- src/components/Admin/Nav/Section.jsx
+++ src/components/Admin/Nav/Section.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
                 </NavLink>
                 <hr/>
                 <ul className="nav nav-pills flex-column mb-auto">
-                    <NavItem link="" title="Dashboard" icon="house-door-fill" />
+                    <NavItem link="/" title="Dashboard" icon="house-door-fill" />
                     <NavItem link="movies" title="Movies" icon="film" />
                 </ul>
             </div>
